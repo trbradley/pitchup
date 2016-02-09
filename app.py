6842f71e34c457459aa60d7b538ad0750bf17967
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2016-02-09 09:11:55 +0000

    add dependencies for karma and protractor in package.json and add minimal scaffold for flask

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+# from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
+
 app = Flask(__name__)
+# app.config.from_object(os.environ['APP_SETTINGS'])
+# db = SQLAlchemy(app)
+
+
+# from models import Result
+
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
 
 if __name__ == '__main__':
     app.run()
--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'super-secret'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
