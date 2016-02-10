edfbdd5541289469cf075344ba3d1cde8d9d273b
Andrew <andy_htun@hotmail.com>
2016-02-10 12:47:48 +0000

    teamsresource tests passing

--- public/js/factories/teamsResourceFactory.js
+++ public/js/factories/teamsResourceFactory.js
@@ -1,10 +1,42 @@
-angular.module('pitchup', ['ngResource']).
-    factory('TeamsResource', function($resource) {
-      var teamsResource = $resource('/teams/', {}, {
-        save: {method: 'GET'}
-      });
+pitchup.factory('TeamsResource', ['$http', '$q', function($http, $q) {
+  var teamsResource = {};
 
-      teamsResource.getTeam = function(id) {
-
-      }
+  teamsResource.getTeam = function(id) {
+    return $http({
+      url: ('/teams/' + id),
+      method: 'GET'
     });
+  };
+
+  teamsResource.getTeams = function() {
+    return $http({
+      url: '/teams',
+      method: 'GET'
+    });
+  };
+
+  // teamsResource.getEnrollments = function(id) {
+  //   return $http({
+  //     url: '/teams/' + id + '/enrollments/new',
+  //     method: 'GET'
+  //   });
+  // };
+
+  teamsResource.postTeams = function(teamName, capacity, numberPlayers) {
+    var deferred = $q.defer();
+    $http.post('/teams', {teamName: teamName, capcity: capacity, numberPlayers: numberPlayers}, { method: 'POST'})
+      .success(function (data, status) {
+        if(status === 200){
+          deferred.resolve(data);
+        } else {
+          deferred.reject();
+        }
+      })
+      .error(function (data) {
+        deferred.reject();
+      });
+    return deferred.promise;
+  };
+
+  return teamsResource;
+}]);
