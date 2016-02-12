9e47132c1f353a732f47a41555fc76007b6426ba
Andrew <andy_htun@hotmail.com>
2016-02-12 11:15:00 +0000

    logout/user controller tests passing

--- public/js/controllers/logoutController.js
+++ public/js/controllers/logoutController.js
@@ -2,8 +2,6 @@ pitchup.controller('LogoutController', ['UserAuth', '$window', function(UserAuth
   var self = this;
 
   self.logout = function() {
-
-    console.log(UserAuth.isLoggedIn());
     UserAuth.logout()
       .then(function() {
         $window.location.href = '/#/teams';
