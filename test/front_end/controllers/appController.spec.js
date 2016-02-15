68d8331d5adadcd1d502feaa57399cb53c98364d
Andrew <andy_htun@hotmail.com>
2016-02-15 17:57:33 +0000

    appCtrl test pending

---
+++ test/front_end/controllers/appController.spec.js
@@ -0,0 +1,25 @@
+describe('AppController', function() {
+  var UserAuth;
+  var currentUser;
+  var ctrl;
+  var scope;
+  var UserAuthFactoryMock;
+
+  beforeEach(function() {
+    UserAuthFactoryMock = jasmine.createSpyObj('UserAuth', ['getCurrentUser']);
+    module('Pitchup', {
+      UserAuth: UserAuthFactoryMock
+    });
+  });
+  beforeEach(inject(function($controller, $q, $rootScope) {
+    UserAuthFactoryMock.getCurrentUser.and.returnValue($q.when({}));
+    ctrl = $controller('AppController');
+    scope = $rootScope;
+  }));
+
+  it('intializes currentUser as undefined', function() {
+    scope.$digest();
+    expect(ctrl.currentUser)
+      .toEqual(undefined);
+  });
+});
