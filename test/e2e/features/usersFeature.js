05d03ba158271a1b664c7f0390b01fc6b45f0b0b
Thomas Bradley <bradley.tom.r@gmail.com>
2016-02-16 20:05:41 +0000

    Finally fixes user feature test and updates travis

--- test/e2e/features/usersFeature.js
+++ test/e2e/features/usersFeature.js
@@ -1,16 +1,6 @@
-var mock = require('protractor-http-mock');
-
 describe('Users Feature', function() {
 
-  beforeEach(function() {
-    mock(['usersFeatureMock.js']);
-  });
-
-  afterEach(function() {
-    mock.teardown();
-  });
-
-  it("allows a user to register, logout, login and view another user's profile", function() {
+  it("allows a user to register, logout, login and view their profile", function() {
     browser.ignoreSynchronization = true;
     browser.get('http://localhost:5000');
 
@@ -18,41 +8,46 @@ describe('Users Feature', function() {
 
     expect(browser.getTitle()).toEqual('PitchUp');
     expect(registerLink.isDisplayed()).toBeTruthy();
-
+    browser.ignoreSynchronization = false;
     registerLink.click();
 
     expect(browser.getCurrentUrl()).toContain('/users/new');
 
-    // var newUserForm = element(by.css('form'));
-    // var usernameInput = element(by.css('input[name="username"]'));
-    // var emailInput = element(by.css('input[name="email"]'));
-    // var passwordInput = element(by.css('input[name="password"]'));
-    //
-    // usernameInput.sendKeys('Mr Example');
-    // emailInput.sendKeys('example@example.com');
-    // passwordInput.sendKeys('password');
-    // newUserForm.submit();
-    //
-    // expect(browser.getCurrentUrl()).toContain('#/teams');
-    // expect(registerLink.isDisplayed()).toBeFalsy();
-    //
-    // var logoutLink = element(by.css('a[href*="#/logout"]'));
-    //
-    // logoutLink.click();
-    //
-    // expect(browser.getCurrentUrl()).toContain('#/teams');
-    // expect(logoutLink.isDisplayed()).toBeFalsy();
-    //
-    // var loginLink = element(by.css('a[href*="#/login"]'));
-    //
-    // loginLink.click();
-    //
-    // expect(browser.getCurrentUrl()).toContain('#/users/1');
-    // expect(loginLink.isDisplayed()).toBeFalsy();
-    //
-    // var userDetails = element(by.css('section[class="description"]'));
-    //
-    // expect(browser.userDetails.getText()).toContain('example@example.com');
+    var newUserForm = element(by.css('form'));
+    var usernameInput = element(by.css('input[name="username"]'));
+    var emailInput = element(by.css('input[name="email"]'));
+    var passwordInput = element(by.css('input[name="password"]'));
 
+    usernameInput.sendKeys('Mr Example');
+    emailInput.sendKeys('example@example.com');
+    passwordInput.sendKeys('password');
+    newUserForm.submit();
+
+    expect(browser.getCurrentUrl()).toContain('/users/1');
+    expect(registerLink.isDisplayed()).toBeFalsy();
+
+    var dropdownMenu = element(by.css('a[class*="dropdown-toggle"]'));
+    dropdownMenu.click();
+    var logoutLink = element(by.css('button[class*="btn btn-link btn-logout"]'));
+    logoutLink.click();
+
+    expect(logoutLink.isDisplayed()).toBeFalsy();
+
+    var loginLink = element(by.css('a[href*="/sessions/new"]'))
+
+    loginLink.click();
+
+    var loginForm = element(by.css('form'));
+
+    usernameInput.sendKeys('Mr Example');
+    passwordInput.sendKeys('password');
+    loginForm.submit();
+
+    expect(browser.getCurrentUrl()).toContain('/users/1');
+    expect(loginLink.isDisplayed()).toBeFalsy();
+
+    var userDetails = element(by.css('section[class*="description"]'));
+
+    expect(userDetails.getText()).toContain('example@example.com');
   });
 });
