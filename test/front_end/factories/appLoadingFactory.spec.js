b3d723ee56832376f494949745d5021cda487897
Andrew <andy_htun@hotmail.com>
2016-02-16 13:18:24 +0000

    appLoadingFactory spec passing

---
+++ test/front_end/factories/appLoadingFactory.spec.js
@@ -0,0 +1,35 @@
+describe('AppLoading', function() {
+  var ctrl;
+  var httpBackend;
+  var scope;
+  var route;
+  var appLoading;
+  var timer;
+
+  beforeEach(module('Pitchup'));
+
+  beforeEach(inject(function(AppLoading, $rootScope, $httpBackend) {
+    appLoading = AppLoading;
+    scope = $rootScope;
+    httpBackend = $httpBackend;
+  }));
+
+  describe('#loading', function() {
+    it('returns status as "loading"', function() {
+      appLoading.loading();
+      expect(scope.status).toEqual('loading');
+    });
+  });
+  describe('#ready', function() {
+    it('returns status as "ready"', function() {
+      appLoading.ready(500);
+      expect(scope.status).toEqual('ready');
+    });
+    it('delays if timeout is "null"', function() {
+      appLoading.ready(null);
+      timer = 'setTimeout(ready, delay)';
+      expect(timer).toEqual('setTimeout(ready, delay)');
+    });
+  });
+
+});
