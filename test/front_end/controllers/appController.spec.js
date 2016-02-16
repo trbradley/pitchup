8818ebd5704de2a28ac3025fca01589b1fda0511
Andrew <andy_htun@hotmail.com>
2016-02-16 10:47:40 +0000

    appController test passing

--- test/front_end/controllers/appController.spec.js
+++ test/front_end/controllers/appController.spec.js
@@ -3,26 +3,35 @@ describe('AppController', function() {
   var currentUser;
   var ctrl;
   var scope;
+  var route;
   var UserAuthFactoryMock;
 
   beforeEach(function() {
     UserAuthFactoryMock = jasmine.createSpyObj('UserAuth', ['getCurrentUser']);
+    routeMock = jasmine.createSpyObj(
+      'routeMock', ['reload']
+    );
     module('Pitchup', {
-      UserAuth: UserAuthFactoryMock
+      UserAuth: UserAuthFactoryMock,
+      $route: routeMock
     });
   });
-  beforeEach(inject(function($controller, $q, $rootScope) {
-    UserAuthFactoryMock.getCurrentUser.and.returnValue($q.when({}));
+  beforeEach(inject(function($controller, $q, $rootScope, $route) {
+    UserAuthFactoryMock.getCurrentUser.and.returnValue($q.when({response: 'user'}));
     ctrl = $controller('AppController');
     scope = $rootScope;
+    route = $route;
   }));
 
   it('intializes currentUser as undefined', function() {
-    scope.$digest();
     expect(ctrl.currentUser)
       .toEqual(undefined);
   });
   it('returns user status on $routeChangeStart', function() {
-    expect(scope.$on).toHaveBeenCalledWith('UserAuth.getCurrentUser');
+    scope.$broadcast("$routeChangeStart");
+    expect(UserAuthFactoryMock.getCurrentUser).toHaveBeenCalled();
+    scope.$digest();
+    expect(ctrl.currentUser)
+      .toEqual({response: 'user'});
   });
 });
