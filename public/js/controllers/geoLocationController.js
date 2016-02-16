1946961d9ea06d0b637fe6baf0f17a281fe2e772
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2016-02-16 08:42:42 +0000

    integrate team pins to map

--- public/js/controllers/geoLocationController.js
+++ public/js/controllers/geoLocationController.js
@@ -1,6 +1,6 @@
 pitchup.controller('GeoLocationController',
-['$geolocation', 'AppLoading', 'uiGmapGoogleMapApi', 'uiGmapIsReady',
-function ($geolocation, AppLoading, uiGmapGoogleMapApi, uiGmapIsReady){
+['$geolocation', 'AppLoading', 'uiGmapGoogleMapApi', 'uiGmapIsReady', 'TeamsResource',
+function ($geolocation, AppLoading, uiGmapGoogleMapApi, uiGmapIsReady, TeamsResource){
   var self = this;
 
   $geolocation.getCurrentPosition()
@@ -15,7 +15,7 @@ function ($geolocation, AppLoading, uiGmapGoogleMapApi, uiGmapIsReady){
         latitude: 51.517339,   // default Makers Academy coords
 				longitude: -0.073337
       },
-      zoom : 13
+      zoom : 12
     };
 
     self.map.center = {
@@ -23,17 +23,33 @@ function ($geolocation, AppLoading, uiGmapGoogleMapApi, uiGmapIsReady){
       longitude: self.location.coords.longitude
     };
 
-    self.icon = 'public/images/pitchup_pin.svg';
-
     self.markers = [
       {
         latitude: self.location.coords.latitude,
         longitude: self.location.coords.longitude,
-        title: 'your position',
-        id: 1,
+        title: 'Your Position',
+        id: 0,
         icon: 'public/images/pitchup_pin_u.svg'
       }
     ];
+    TeamsResource.getTeams()
+    .then(function(response) {
+      response.data.teams.forEach(function(team) {
+        var geocoder = new maps.Geocoder();
+        geocoder.geocode( {'address': team.pitch_postcode}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            var coords = results[0].geometry.location;
+            self.markers.push({
+              latitude: coords.lat(),
+              longitude: coords.lng(),
+              title: team.name,
+              id: team.id,
+              icon: 'public/images/pitchup_pin.svg'
+            });
+          }
+        });
+      });
+    });
   });
 
   uiGmapIsReady.promise()
--- public/js/controllers/newTeamController.js
+++ public/js/controllers/newTeamController.js
@@ -1,16 +1,18 @@
 pitchup.controller('NewTeamController',
-['TeamsResource', '$location', '$route',
-function(TeamsResource, $location, $route) {
+['TeamsResource', '$location', '$route', '$filter',
+function(TeamsResource, $location, $route, $filter) {
   var self = this;
 
   self.createNewTeam = function() {
+    date_formatted = $filter('date')(self.date, 'yyyy-MM-dd');
+    time_formatted = $filter('date')(self.time, 'HH:mm');
     TeamsResource.postTeams(
       self.teamName,
       self.capacity,
       self.numberPlayers,
       self.pitchPostcode,
-      self.date,
-      self.time
+      date_formatted,
+      time_formatted
     )
     .then(function(response) {
       $location.path('/teams');
