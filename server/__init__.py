ec2c859975a3b474ff14f01ab36f781aefbd314c
tmgree <tmgree@outlook.com>
2016-02-17 16:33:26 +0000

    flask mail

--- server/__init__.py
+++ server/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask, send_from_directory, session
 from flask.ext.session import Session
 from flask.ext.restful import Api
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 
 ################
@@ -35,6 +36,39 @@ import server.controllers.users
 import server.controllers.teams
 import server.controllers.sessions
 import server.controllers.enrollments
+
+
+app.config.update(
+	DEBUG=True,
+	#EMAIL SETTINGS
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=465,
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME = 'pitchuphelp@gmail.com',
+	MAIL_PASSWORD = 'tombradley1234'
+	)
+
+mail=Mail(app)
+
+@app.route("/send-mail/")
+def index():
+	msg = Message(
+              'Hello',
+	       sender='pitchuphelp@gmail.com',
+	       recipients=
+               ['tmgree@outlook.com'])
+	msg.body = "This is the email body"
+	msg.html = "<b>This is the email body</b>"
+	mail.send(msg)
+	return "Sent"
+
+
+
+
+
+
+
+
 
 
 @app.route('/')
